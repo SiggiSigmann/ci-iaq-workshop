096c5d0492ad5e87bd3c731ed506ac7934206927
SiggiSigmann <siggisigmann@googlemail.com>
2022-06-20 15:46:56 +0200

    Update test_heroapp.py

--- test/test_heroapp.py
+++ test/test_heroapp.py
@@ -16,7 +16,7 @@ def test_heroapp_container_is_running(host):
 
 def test_heroapp_is_available_on_port_80(host):
     # testen, ob auf tcp://0.0.0.0:80 gehorcht wird
-    server = host.addr("tcp://0.0.0.0")
-    assert server.port(80).is_reachable
+    server = host.socket("tcp://0.0.0.0:80")
+    assert server.is_listening
 
     
