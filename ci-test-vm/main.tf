c57b843662926db8a7001b11c86af21fc74973a6
SiggiSigmann <siggisigmann@googlemail.com>
2022-06-20 15:40:24 +0200

    Update main.tf

--- ci-test-vm/main.tf
+++ ci-test-vm/main.tf
@@ -5,7 +5,7 @@ provider "hcloud" {
 
 # Create a new SSH key
 resource "hcloud_ssh_key" "ansible-test-ssh-key" {
-  name = "ssh-ts"
+  name = "ssh-ts1"
   public_key = file(var.ssh_key)
 }
 
@@ -16,7 +16,7 @@ resource "hcloud_server" "ansible-test-vm" {
   server_type = "cx11"
   location = "nbg1"
   ssh_keys = [
-    "ssh-ts"
+    "ssh-ts1"
   ]
 
   provisioner "local-exec" {
