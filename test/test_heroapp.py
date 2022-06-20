3abc0a886b9f01142e7ea0a5788729ccd89402b7
SiggiSigmann <siggisigmann@googlemail.com>
2022-06-20 14:57:40 +0200

    Update test_heroapp.py

--- test/test_heroapp.py
+++ test/test_heroapp.py
@@ -1,11 +1,20 @@
 def test_docker_is_installed(host):
     # testen, ob das Paket "docker-ce" installiert ist
+    docker_ce = host.package("docker-ce")
+    assert docker_ce.is_installed
 
 def test_docker_service_is_running(host):
     # testen, ob der Service "docker" läuft und verfügbar ist
+    docker = host.service("docker")
+    assert docker.is_running
+    assert docker.is_enabled
 
 def test_heroapp_container_is_running(host):
     # testen, ob der container mit dem Namen "my-hero-app" läuft
+    my_hero_app = host.docker("my-hero-app")
+    assert my_hero_app.is_running()
 
 def test_heroapp_is_available_on_port_80(host):
     # testen, ob auf tcp://0.0.0.0:80 gehorcht wird
+    server = host.addr("tcp://0.0.0.0")
+    assert server.port(80).is_reachable
